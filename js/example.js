7bae5064f54aafc8c2b340644dd1dcd16cb238df
IceCrystal21 <master0sword@gmail.com>
2025-10-14 18:22:39 -0400

    w

--- js/example.js
+++ js/example.js
@@ -1,8 +1,10 @@
 document.getElementById("push").addEventListener("click", function() {
     var age = document.getElementById("input")
+    var num = document.getElementById("num")
     if (age > 18) {
         alert("You cannot vote")
     } else {
         alert("You can vote")
     }
+    num.innerText = String(age)
 })
