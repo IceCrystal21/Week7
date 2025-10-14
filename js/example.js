e79af7767ab705afe15b693c08263dfa3dad9428
IceCrystal21 <master0sword@gmail.com>
2025-10-14 17:59:12 -0400

    asndASJdcasd

--- js/example.js
+++ js/example.js
@@ -1,1 +1,9 @@
-console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEY IM WALKING HERE")
+let age;
+var name;
+console.log(age)
+console.log(name)
+
+for (let i=0; i<10; 1++) {
+    console.log("Inside scope", i)
+}
+console.log("Oustside scope", i)
